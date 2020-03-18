b7f2b354507cefb5182363fa9117ff9c1805f7b4
Roger Molas <r.molas06@gmail.com>
2020-03-18 18:00:50 +0800

    sample script

---
+++ myfirstscript.r
@@ -0,0 +1,12 @@
+library(dplyr)
+library(tidyverse)
+library(magrittr)
+library(dslabs)
+
+data(murders)
+
+murders %>%
+  ggplot( aes(population, total, label = abb, color = region) ) + 
+  geom_label()
+
+
